76fdf3e76ad19d7679bf66f5199aec237b023357
Aashay Bhoir <contact@aashaybhoir.me>
2020-12-27 23:57:50 +0530

    Resolved merge Conflicts

--- app/src/main/java/com/blacklimon/landing_seeker/ui/MainActivity.kt
+++ app/src/main/java/com/blacklimon/landing_seeker/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import com.blacklimon.landing_seeker.R
 
 class MainActivity: AppCompatActivity() {
 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
